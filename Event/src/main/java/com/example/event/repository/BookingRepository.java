0ca02c17b7bc3efde2b3ad3f261b43c8cc5464ec
240850320041_Mohammad Imamuddin <miuddin4626@gmail.com>
2025-02-04 11:21:20 +0530

    deleted unncessary file

--- Event/src/main/java/com/example/event/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.event.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.event.entity.Booking;
-
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
-
-}
