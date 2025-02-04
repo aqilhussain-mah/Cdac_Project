4b3c37c90f473ddd9f175cb42303988f53f30d72
aqilhussain-mah <aqilhussain003@gmail.com>
2025-02-05 00:45:14 +0530

    removed Event

--- Event/src/main/java/com/example/event/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.event.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
